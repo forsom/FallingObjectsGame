3d344f08f7da1779aefb91a4c0738f75a77cd099
Mark <markbojko85@gmail.com>
2025-04-17 01:06:31 +0300

    commit
    Replaced Invoke with Coroutine, removed unnecessary usings, added game difficulty increase logic.

--- Assets/Scripts/FallingObjects/ObjectFallController.cs
+++ Assets/Scripts/FallingObjects/ObjectFallController.cs
@@ -1,74 +1,75 @@
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-using Unity.VisualScripting;
 
 public class ObjectFallControler : MonoBehaviour
 {
-    [SerializeField] PlayerMovement playerMovement;
-    [SerializeField] float minX = -20f;
-    [SerializeField] float maxX = 20f;
-    [SerializeField] private Button easyButton;
-    [SerializeField] private Button normalButton;
-    [SerializeField] private Button hardButton;
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private float _minX = -20f;
+    [SerializeField] private float _maxX = 20f;
+    [SerializeField] private float _wait = 0.8f;
+    [SerializeField] private float _waitDecrease = 0.05f;
+    [SerializeField] private float _increaseDifficultyInterval = 5f;
     public GameObject fallingObject;
-    private List<GameObject> spawnedObjects = new List<GameObject>();
-    public float _wait = 0.8f;
+    private List<GameObject> _spawnedObjects = new List<GameObject>();
+    private float _timeSinceLastIncreasedDiffuculty;
+    private bool _isSpawning = true;
 
 
-    void Start()
+    private void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+    void Update()
+    {
+        IncreaseDifficulty();
+    }
+
+    private void RandomDirectionSpawn()
+    {
+        GameObject newobject = Instantiate(fallingObject, new Vector3(Random.Range(_minX, _maxX), 15, 0), Quaternion.identity);
+        _spawnedObjects.Add(newobject);
+    }
+    private System.Collections.IEnumerator Spawner()
     {
         if (playerMovement != null && fallingObject != null)
         {
             playerMovement.PlayerDied += StopObjectsFall;
-            InvokeRepeating("RandomFall", _wait, _wait);
+            while (_isSpawning)
+            {
+                RandomDirectionSpawn();
+                yield return new WaitForSeconds(_wait);
+            }
         }
     }
-    private void RandomFall()
-    {
-        GameObject newobject = Instantiate(fallingObject, new Vector3(Random.Range(minX, maxX), 15, 0), Quaternion.identity);
-        spawnedObjects.Add(newobject);
-    }
-    void StopObjectsFall()
+    private void StopObjectsFall()
     {
         if (playerMovement != null)
         {
-            CancelInvoke("RandomFall");
+            _isSpawning = false;
             playerMovement.PlayerDied -= StopObjectsFall;
             HideObjects();
         }
     }
-    public void ChangeGameDiffuculty0()
+
+    private void HideObjects()
     {
-        Debug.Log("0");
-        _wait = 0.8f;
-        InvokeRepeating("RandomFall", _wait, _wait);
-        HideObjects();
-    }
-    public void ChangeGameDiffuculty1()
-    {
-        _wait = 0.4f;
-        InvokeRepeating("RandomFall", _wait, _wait);
-        HideObjects();
-    }
-    public void ChangeGameDiffuculty2()
-    {
-        _wait = 0.2f;
-        InvokeRepeating("RandomFall", _wait, _wait);
-        HideObjects();
-    }
-    public void HideObjects()
-    {
-        foreach (GameObject obj in spawnedObjects)
+        foreach (GameObject obj in _spawnedObjects)
         {
             if (obj != null)
             {
                 obj.SetActive(false);
             }
         }
-        spawnedObjects.Clear();
+        _spawnedObjects.Clear();
+    }
+    private void IncreaseDifficulty()
+    {
+        _timeSinceLastIncreasedDiffuculty += Time.deltaTime;
+        if (_timeSinceLastIncreasedDiffuculty >= _increaseDifficultyInterval)
+        {
+            _wait = Mathf.Max(_waitDecrease,_wait - _waitDecrease);
+            _timeSinceLastIncreasedDiffuculty = 0f;
+        }
     }
 }
 
